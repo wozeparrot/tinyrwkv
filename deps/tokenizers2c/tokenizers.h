a4d7de410adec38694939049f528829684f6a300
Woze Parrot <wozeparrot@gmail.com>
2023-04-28 00:47:53 -0400

    feat: c wrapper for tokenizers

---
+++ deps/tokenizers2c/tokenizers.h
@@ -0,0 +1,7 @@
+void *tk_from_file(const char *file);
+
+void tk_free(void *tokenizer);
+
+unsigned int *tk_encode(void *tokenizer, const char *text, unsigned int *len);
+
+char *tk_decode(void *tokenizer, const unsigned int *tokens, unsigned int len);
