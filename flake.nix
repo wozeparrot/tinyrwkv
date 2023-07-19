788b4706de8e5cf9bbd5c729f511d7cea500c5e2
Woze Parrot <wozeparrot@gmail.com>
2023-07-19 01:11:48 -0400

    feat: webgpu support

--- flake.nix
+++ flake.nix
@@ -34,50 +34,93 @@
             targets."x86_64-unknown-linux-musl".latest.rust-std
           ];
       in {
-        devShell = pkgs.mkShell {
-          packages = let
-            python-packages = p: let
-              tinygrad = p.buildPythonPackage {
-                pname = "tinygrad";
-                version = inputs.tinygrad.shortRev;
-                src = inputs.tinygrad;
-                doCheck = false;
-                propagatedBuildInputs = with p; [
-                  networkx
-                  numpy
-                  pillow
-                  pyopencl
-                  pyyaml
-                  requests
-                  tqdm
-                ];
+        devShell = let
+          wgpu-native = pkgs.rustPlatform.buildRustPackage rec {
+            pname = "wgpu-native";
+            version = "0.14.2.3";
+            src = pkgs.fetchFromGitHub {
+              owner = "gfx-rs";
+              repo = pname;
+              rev = "v${version}";
+              fetchSubmodules = true;
+              sha256 = "sha256-MkWU5STYKmyA2EamwXtGzmz4dcL1aty1NkG8yOylUR8=";
+            };
+            cargoLock = {
+              lockFile = ./nix/wgpu-native_Cargo.lock;
+              outputHashes = {
+                "naga-0.10.0" = "sha256-CqsIPHHdrzollxhc6/eFSf91nBiHzMuGm4CxsKagKMo=";
+                "wgpu-core-0.14.2" = "sha256-UNJ9rD6vFOja6g9/7hxR5u3cl8rwpjlT8UPrZI4azgE=";
               };
-            in
-              with p; [
-                llvmlite
-                mypy
-                numpy
-                pydot
-                tinygrad
-                tokenizers
-                torch
-                tqdm
-                wandb
-                mido
-              ];
-            llvmPackages = pkgs.llvmPackages_15;
-            python = pkgs.python310;
-          in
-            with pkgs; [
-              (python.withPackages python-packages)
-              rust-toolchain
-              llvmPackages.clang
-              llvmPackages.libunwind
-
-              # needed for GRAPH=1 to work
-              graphviz
+            };
+            nativeBuildInputs = with pkgs; [
+              rustPlatform.bindgenHook
             ];
-        };
+          };
+        in
+          pkgs.mkShell {
+            packages = let
+              python-packages = p: let
+                wgpu-py = p.buildPythonPackage rec {
+                  pname = "wgpu-py";
+                  version = "0.9.4";
+                  src = pkgs.fetchFromGitHub {
+                    owner = "pygfx";
+                    repo = pname;
+                    rev = "v${version}";
+                    sha256 = "sha256-1h+l4Fa3ooM1ifUjSyte6qCa1LrS2XBlkcHTBDCR9Kw=";
+                  };
+                  doCheck = false;
+                  propagatedBuildInputs = with p; [
+                    cffi
+                  ];
+                };
+                tinygrad = p.buildPythonPackage {
+                  pname = "tinygrad";
+                  version = inputs.tinygrad.shortRev;
+                  src = inputs.tinygrad;
+                  doCheck = false;
+                  propagatedBuildInputs = with p; [
+                    networkx
+                    numpy
+                    pillow
+                    pyopencl
+                    pyyaml
+                    requests
+                    tqdm
+                  ];
+                };
+              in
+                with p; [
+                  llvmlite
+                  mido
+                  mypy
+                  numpy
+                  pydot
+                  tinygrad
+                  tokenizers
+                  torch
+                  tqdm
+                  wandb
+                  wgpu-py
+                ];
+              llvmPackages = pkgs.llvmPackages_15;
+              python = pkgs.python310;
+            in
+              with pkgs; [
+                (python.withPackages python-packages)
+                rust-toolchain
+                llvmPackages.clang
+                llvmPackages.libunwind
+
+                # needed for GRAPH=1 to work
+                graphviz
+              ];
+
+            shellHook = ''
+              export WGPU_LIB_PATH=${wgpu-native}/lib/libwgpu_native.so
+              export LD_LIBRARY_PATH=${pkgs.vulkan-loader}/lib
+            '';
+          };
       }
     );
 }
